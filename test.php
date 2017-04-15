763c15b37650436331716fed847dceb69f8e884b
wubiao239 <wubiao239@163.com>
2017-04-15 17:28:05 +0800

    测试图

--- test.php
+++ test.php
@@ -3,78 +3,69 @@
 //不限执行时间
 set_time_limit(0); 
 error_reporting(0);
+abstract class SearchInterface   
+{  
+    protected $G;//图  
+    protected $s;//图的首节点  
+    function __construct($_G,$_s){$this->G = $_G;$this->s = $_s;}  
+    public abstract function search();  
+}  
 
-function fetchlinks($url)
-{	
-	$links=array();
-	if ($content=file_get_contents($url))
-	{			
-		$links= striplinks($content);
-		$links= expandlinks($links, $url);
+class bfs extends SearchInterface  
+{  
+    private $d = array();//源点s和顶点u之间的距离  
+    private $tt = array();//结点u的父母存于变量  
+    private $visit = array();//已访问节点  
+      
+    function __construct($_G,$_s)  
+    {  
+        parent::__construct($_G,$_s);  
+          
+        //初始化$d/$tt,初始值为无穷大/NULL  
+        for($i=0;$i<9;$i++)  
+        {  
+            $this->d[$i] = 20000;  
+            $this->tt[$i] = NULL;  
+            $this->visit[$i] = 0;  
+        }  
+          
+    }  
+    public function search()  
+    {  
+        //访问所有节点  
+          
+        $queue = array();  
+        for($i=0;$i<9;$i++)  
+        {  
+            if($this->visit[$i]==0)  
+            {  
+                array_push($queue,$i);  
+                while(!empty($queue))  
+                {  
+                    $_s = array_shift($queue);                    
+                    $this->visit[$_s] = 1;  
+                    echo ($_s+1).'<br>';  
+                      
+                    $link_s = $this->G->get_links($_s);  
+                    //获取和s直接相连的顶点u  
+                      
+                    foreach($link_s as $j => $u)  
+                    {  
+                        if($this->visit[$u]==0)  
+                        {  
+                            array_push($queue,$u);  
+                            $this->visit[$u] = 2;      
+                        }  
+                    }  
+                }  
+            }  
+        }  
+    }  
+  
+}  
 
-		return $links;
-	}
-	else
-		return false;
-}
-
-function expandlinks($links,$url)
-{
-	
-	preg_match("~^[^\?]+~",$url,$match);
-
-	$match = preg_replace("~/[^\/\.]+\.[^\/\.]+$~","",$match[0]);
-	$match = preg_replace("~/$~","",$match);
-	$match_part = parse_url($match);
-	$match_root =$match_part["scheme"]."://".$match_part["host"];
-			
-	$search = array( 	"~^http://".preg_quote($match_part["host"])."~i",
-						"~^(\/)~i",
-						"~^(?!http://)(?!mailto:)~i",
-						"~/\./~",
-						"~/[^\/]+/\.\./~"
-					);
-					
-	$replace = array(	"",
-						$match_root."/",
-						$match."/",
-						"/",
-						"/"
-					);			
-	foreach ($links as $key => $value) {
-		$expandedLinks[] = preg_replace($search,$replace,$value);
-	}		
-	
-
-	return $expandedLinks;
-}
-
-function striplinks($document)
-{	
-	preg_match_all("~<\s*a\s.*?href\s*=\s*			# 查找 <a href=
-					([\"\'])?					# 查找单双引号
-					(?(1) (.*?)\\1 | ([^\s\>]+))		# if quote found, match up to next matching
-												# quote, otherwise match up to next space
-					~isx",$document,$links);
-					
-
-	while(list($key,$val) = each($links[2]))
-	{
-		if(!empty($val))
-			$match[] = $val;
-	}				
-	
-	while(list($key,$val) = each($links[3]))
-	{
-		if(!empty($val))
-			$match[] = $val;
-	}		
-	
-	return $match;
-}
-
-$links=fetchlinks("http://www.hobokenremax.pw/");
-print_r($links);
-
+$G = new Graphic;  
+$search = new bfs($G,1);  
+$search->search();  
 
 ?>
