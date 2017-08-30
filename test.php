494b54ddfd58529571ac215483da8d72d51c9798
wubiao <wubiao239@163.com>
2017-08-30 16:00:39 +0800

    调整结构

--- test.php
+++ test.php
@@ -4,23 +4,7 @@
 //不限执行时间
 set_time_limit(0); 
 error_reporting(0);
-//从日志文件中获取url
-function getUrls($fileName){
-	$lineArray=array();
-	$fp = fopen($fileName, "rb") or die("Unable to open file ".$fileName);
-	
-	while(! feof($fp))
-	{
-		$line=fgets($fp);
-		$line=trim($line);
-		echo $line;
 
-	}
-
-	fclose($fp);
-	
-
-}
-getUrls("test.sql");
+echo __FILE__;
 
  ?>
